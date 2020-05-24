52bd39d204d812d4ac4d3c16018f32c11bbd373c
mobilevisuals <eyvind@mobile-visuals.com>
2020-05-24 19:18:27 +0200

    234234

---
+++ src/main/java/com/example/sec3mvc/Sec3mvcApplication.java
@@ -0,0 +1,13 @@
+package com.example.sec3mvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sec3mvcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Sec3mvcApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/sec3mvc/Sec3mvcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sec3mvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sec3mvcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
